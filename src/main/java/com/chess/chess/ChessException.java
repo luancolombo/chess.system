b5848d12c81c831311533c21f4033c3f2a8b6ac3
Luan Colombo <luanbelly@gmail.com>
2023-07-17 21:16:25 +0100

    ChessException and ChessPosition

---
+++ src/main/java/com/chess/chess/ChessException.java
@@ -0,0 +1,9 @@
+package com.chess.chess;
+
+public class ChessException extends RuntimeException{
+    private static final long SerialVersionUID =1L;
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
